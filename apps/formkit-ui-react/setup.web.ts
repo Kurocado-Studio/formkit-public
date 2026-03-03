871ee190135be4933bf1ff90826b866f12252bd5
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2026-03-02 22:01:21 -0600

    fix(config): fixes eslint warning

--- apps/formkit-ui-react/setup.web.ts
+++ apps/formkit-ui-react/setup.web.ts
@@ -1,2 +1,6 @@
-import '@kurocado-studio/qa/web/setup';
-import '@testing-library/jest-dom';
+(globalThis as typeof globalThis & { global?: typeof globalThis }).global ??=
+  globalThis;
+// @ts-expect-error while QA package gets updated
+await import('@kurocado-studio/qa/web/setup');
+// @ts-expect-error while QA package gets updated
+await import('@testing-library/jest-dom');
